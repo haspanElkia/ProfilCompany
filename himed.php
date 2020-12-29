b50c5a713b220e0961c0c8eed39f94d0c522f1ea
anggr14n <anggr14n@gmail.com>
2020-12-29 11:28:04 +0700

    first commit

---
+++ himed.php
@@ -0,0 +1,338 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- Css sendiri -->
+    <link rel="stylesheet" href="style.css">
+    <title>My Profile</title>
+</head>
+
+<body>
+    <!-- 5. Awal Section Our Services -->
+    <section class="bgp">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 center">
+                    <img src="img/section-icon-2.svg" alt="***">
+                    <h2>Our Core Features</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <img src="img/about1.svg" alt="" class="center-block img-responsive" style="width:100%">
+                </div>
+                <div class="col-lg-6 right">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="img/feature1.svg" alt="..." class="feature">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="text-feature">Measure The Customer Experience</h5>
+                                    <p class="text-feature">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium modi assumenda beatae.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- 5. Akhir Section Our Services -->
+
+    <!-- 6. Awal Section Our Services -->
+    <section class="bgp">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 center">
+                    <img src="img/section-icon-3.svg" alt="***">
+                    <h2>Our Services</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                </div>
+            </div>
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col-lg-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/s2.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/s2.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/s2.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row row-cols-1 row-cols-md-2 g-4 mt-auto">
+                <div class="col-lg-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/s2.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/s2.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/s2.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- 6. Akhir Section Our Services -->
+
+    <!-- 7. Awal Section Frequently Questions -->
+    <section class="bgp">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-12 center">
+                    <img src="img/section-icon-5.svg" alt="***">
+                    <h2>Frequently Questions</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <img src="img/about3.svg" alt="" class="center-block img-responsive" style="width:100%">
+                </div>
+                <div class="col-lg-6 right">
+                    <p>
+                        <div class="badge bg-primary text-wrap p-3">
+                            What are the objectives of this Software?
+                        </div>
+                    </p>
+                    <p>
+                        <div class="badge bg-primary text-wrap p-3">
+                            What is the best features and services we deiver?
+                        </div>
+                    </p>
+                    <p>
+                        <div class="badge bg-primary text-wrap p-3">
+                            Why this app important to me?
+                        </div>
+                    </p>
+                    <p>
+                        <div class="badge bg-primary text-wrap p-3">
+                            how may I take part in and purchase this Software?
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Section Frequently Questions -->
+
+    <!-- 8. Awal Section Frequently Questions -->
+    <section class="bgp">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-12 center">
+                    <img src="img/section-icon-2.svg" alt="***">
+                    <h2>Our Pricing Plans</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                </div>
+            </div>
+            <div class="row">
+                <!-- Single Table -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="single_price_table_content text-center wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                        <div class="price_table_text">
+                            <h5>Begginer</h5>
+                            <h1>Free</h1>
+                            <p>Always</p>
+                        </div>
+                        <div class="table_text_details">
+                            <p>250GB Bandwidth</p>
+                            <p>08 Email Account</p>
+                            <p>Unlimited Database </p>
+                            <p>10GB Free Disk </p>
+                            <p>24/7 Support</p>
+                        </div>
+                        <div class="table_btn mt-50">
+                            <a href="#" class="btn dream-btn">Start</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Table -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="single_price_table_content active text-center wow fadeIn shadow p-3 mb-5 bg-white rounded" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeIn;">
+                        <div class="price_table_text">
+                            <h5>Business</h5>
+                            <h1>$24.99</h1>
+                            <p>Per Month</p>
+                        </div>
+                        <div class="table_text_details">
+                            <p>250GB Bandwidth</p>
+                            <p>08 Email Account</p>
+                            <p>Unlimited Database </p>
+                            <p>10GB Free Disk </p>
+                            <p>24/7 Support</p>
+                        </div>
+                        <div class="table_btn mt-50">
+                            <a href="#" class="btn dream-btn">Start</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Table -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="single_price_table_content text-center wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <div class="price_table_text">
+                            <h5>professional</h5>
+                            <h1>$84.99</h1>
+                            <p>Per Month</p>
+                        </div>
+                        <div class="table_text_details">
+                            <p>250GB Bandwidth</p>
+                            <p>08 Email Account</p>
+                            <p>Unlimited Database </p>
+                            <p>10GB Free Disk </p>
+                            <p>24/7 Support</p>
+                        </div>
+                        <div class="table_btn mt-50">
+                            <a href="#" class="btn dream-btn">Start</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Section Frequently Questions -->
+
+    <!-- 9. Awal Section Loved By Our Clients -->
+    <section class="bgp">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-12 center">
+                    <h2>Loved By Our Clients</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container kar">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col-md-4">
+                    <div class="card  p-5">
+                        <div class="card-body">
+                            <img src="img/1.jpg" class="card-img-top rounded-circle" alt="...">
+                            <hr>
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card  p-5">
+                        <div class="card-body">
+                            <img src="img/2.jpg" class="card-img-top rounded-circle" alt="...">
+                            <hr>
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4" id="1">
+                    <div class="card  p-5">
+                        <div class="card-body">
+                            <img src="img/3.jpg" class="card-img-top rounded-circle" alt="...">
+                            <hr>
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row pt-4">
+                <div class="col-md-12">
+                    <a href="#1" class="btn btn-success rounded-circle">01</a>
+                    <a href="#2" class="btn btn-danger rounded-circle">02</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Section Loved By Our Clients -->
+
+    <!-- 10. Awal Section Loved By Our Clients Footer -->
+    <section class="bgp">
+        <div class="container">
+            <div class="row shadow-sm p-3 mb-5 bg-primary rounded">
+                <div class="col-md-9">
+                    <h2>So What s Next?</h2>
+                    <h1>Are You Ready? Let’S Work!</h1>
+                </div>
+                <div class="col-md-3 tengah">
+                    <a href="#" class="btn btn-danger">READ MORE</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae quae distinctio facere, nisi labore ratione maxime dolore alias accusantium. Temporibus accusamus laudantium reprehenderit a fugiat nostrum voluptatum dicta molestiae cupiditate.
+                </div>
+                <div class="col-md-3">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae quae distinctio facere, nisi labore ratione maxime dolore alias accusantium. Temporibus accusamus laudantium reprehenderit a fugiat nostrum voluptatum dicta molestiae cupiditate.
+                </div>
+                <div class="col-md-3">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae quae distinctio facere, nisi labore ratione maxime dolore alias accusantium. Temporibus accusamus laudantium reprehenderit a fugiat nostrum voluptatum dicta molestiae cupiditate.
+                </div>
+                <div class="col-md-3">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae quae distinctio facere, nisi labore ratione maxime dolore alias accusantium. Temporibus accusamus laudantium reprehenderit a fugiat nostrum voluptatum dicta molestiae cupiditate.
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Section Foter -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
